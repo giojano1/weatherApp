76b8534b374b6ea388d6270cc624bcbcae21d39e
giojano1 <giorgi.janelidze64@gmail.com>
2024-05-20 01:42:00 +0400

    working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,47 @@
-// Your API key from weatherapi.com
+const input = document.querySelector("#searchBar");
+const searchBtn = document.querySelector("#searchBtn");
+const temp = document.querySelector("#temp");
+const clouds = document.querySelector("#clouds");
+const img = document.querySelector("#img");
+const humidity = document.querySelector("#humidity");
+const wind = document.querySelector("#wind");
 const apiKey = "2b7a339c7a61413a8cf182354241905";
-
-// The endpoint for current weather data
 const endpoint = "http://api.weatherapi.com/v1/current.json";
+searchBtn.addEventListener("click", search);
+function search() {
+  let city = input.value;
+  const url = `${endpoint}?key=${apiKey}&q=${city}`;
+  fetch(url)
+    .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+      const weather = data.current;
 
-// The city for which you want to fetch the weather
-const city = "London";
-
-// Construct the full URL with the endpoint, API key, and city
-const url = `${endpoint}?key=${apiKey}&q=${city}`;
-
-// Fetch the data
-fetch(url)
-  .then((response) => {
-    // Check if the response is ok (status code 200-299)
-    if (!response.ok) {
-      throw new Error("Network response was not ok " + response.statusText);
-    }
-    return response.json(); // Parse the JSON from the response
-  })
-  .then((data) => {
-    // Handle the data
-    console.log(data);
-    // You can now use the weather data, for example:
-    const weather = data.current;
-    console.log(`Temperature in ${city}: ${weather.temp_c}°C`);
-  })
-  .catch((error) => {
-    // Handle any errors that occur during fetch
-    console.error("There has been a problem with your fetch operation:", error);
-  });
+      temp.textContent = `${weather.temp_c}°C`;
+      clouds.textContent = weather.condition.text;
+      humidity.textContent = `${weather.humidity}%`;
+      wind.textContent = `${weather.wind_kph}Km/h`;
+      if (
+        weather.condition.text === "Clear" ||
+        weather.condition.text === "Sunny"
+      ) {
+        img.src = "assets/img/clear.png";
+      } else if (
+        weather.condition.text === "Partly cloudy" ||
+        weather.condition.text === "Overcast"
+      ) {
+        img.src = "assets/img/cloud.png";
+      } else if (
+        weather.condition.text === "Patchy light rain with thunder" ||
+        weather.condition.text === "Light rain" ||
+        weather.condition.text === "Moderate rain"
+      ) {
+        img.src = "assets/img/rain.png";
+      } else if (weather.condition.text === "Mist") {
+        img.src = "assets/img/mist.png";
+      } else if (weather.condition.text === "Light Snow") {
+        img.src = "assets/img/snow.png";
+      }
+    });
+}
